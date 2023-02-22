6995cc14941e7aed64109507742baf8ed140c809
Edward <edward@synap.ac>
2023-02-22 17:00:38 +0000

    Patch for synap

--- src/GraphQL/parseGraphQLUtils.js
+++ src/GraphQL/parseGraphQLUtils.js
@@ -1,5 +1,5 @@
+import { GraphQLYogaError } from '@graphql-yoga/node';
 import Parse from 'parse/node';
-import { GraphQLError } from 'graphql';
 
 export function enforceMasterKeyAccess(auth) {
   if (!auth.isMaster) {
@@ -16,7 +16,7 @@ export function toGraphQLError(error) {
     code = Parse.Error.INTERNAL_SERVER_ERROR;
     message = 'Internal server error';
   }
-  return new GraphQLError(message, { extensions: { code } });
+  return new GraphQLYogaError(message, { code, parseError: error });
 }
 
 export const extractKeysAndInclude = selectedFields => {
